d840f2506667432c770f978efc549fecf1cd406f
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-04-27 15:22:54 +0200

    move config, remove .env

--- src/postgresClient.ts
+++ src/postgresClient.ts
@@ -1,19 +1,28 @@
-import dotenv from 'dotenv';
 import { Client } from 'ts-postgres';
 
-
-dotenv.config();
-
-export const postgresSomething = process.env.POSTGRES_SOMETHING;
-
 export async function markAsRecommended(id: string, marked: boolean): Promise<string | null> {
-    const client = new Client({host: 'localhost', user: 'collections', password: 'collections', database: 'search'});
+    const client = new Client({
+        host: 'localhost',
+        user: 'collections',
+        password: 'collections',
+        database: 'search',
+    });
     await client.connect();
     let result: string | null;
-    if(marked){
-        result = (await client.query('INSERT INTO "collections_references" VALUES ($1,$2,now(),null)', ['EDITORIAL', id])).status;
+    if (marked) {
+        result = (
+            await client.query('INSERT INTO "collections_references" VALUES ($1,$2,now(),null)', [
+                'EDITORIAL',
+                id,
+            ])
+        ).status;
     } else {
-        result = (await client.query('DELETE FROM "collections_references" WHERE collection_uuid = $1 AND reference_uuid = $2', ['EDITORIAL', id])).status;
+        result = (
+            await client.query(
+                'DELETE FROM "collections_references" WHERE collection_uuid = $1 AND reference_uuid = $2',
+                ['EDITORIAL', id],
+            )
+        ).status;
         await client.query('UPDATE "references" SET last_updated = now() WHERE uuid = $1', [id]);
     }
     return result;
